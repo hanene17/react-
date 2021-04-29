53431ed5a3e80f5c35b4a9365f76cd7aa2ad85e7
hanene <ghribihanen17@gmail.com>
2021-04-29 18:52:55 +0100

    code

--- src/App.js
+++ src/App.js
@@ -3,13 +3,53 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'; 
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import React from 'react';
+import ReactDom from 'react-dom';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
  function App(){
+ 
+
    return (
+    
+   
  <div className="App">
-    <header className="App-header">
+ 
+ 
+<Navbar sticky="top" />
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+      <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+      </NavDropdown>
+    </Nav>
+    <Nav>
+      <Nav.Link href="#deets">More deets</Nav.Link>
+      <Nav.Link eventKey={2} href="#memes">
+        Dank memes
+      </Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>
+        
+ <header className="App-header">
+ 
+    
     <img src={logo} className="App-logo" alt="logo" />
+    
  <Form>
+
   <Form.Group controlId="formBasicEmail">
     <Form.Label>Email address</Form.Label>
     <Form.Control type="email" placeholder="Enter email" />
@@ -28,11 +68,24 @@ import Button from 'react-bootstrap/Button';
   <Button variant="primary" type="submit">
     Submit
   </Button>
+  
 </Form>
+
 </header>
-    </div>
-   );
+<div id="first-app">My First App!! ❤️ <code>React</code> let's get started.</div>
+
+</div>
+
+   
+  
+);
+
+
  }
-export default App;
+
+ export default App
 
 
+
+ 
+ 
